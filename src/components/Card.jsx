f1355001c6cd4f4da603c375de64310bcc4951d7
Camila Arenas <mcarenasdh@gmail.com>
2025-08-19 12:34:46 +0200

    Create handleClick function

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,17 @@
 import "./Card.css";
 import { Link } from "react-router-dom";
 
-function Card ({card}) {
+function Card({ card, onCardClick }) {
+  const handleClick = () => {
+    if (onCardClick) {
+      onCardClick(card);
+    }
+  };
 
   return (
     <div>
       <div className="cards-container">
-          <Link to={`/card/${card.id}`} key={card.id}>
-            <div className="card-back">
-            </div>
-          </Link>
+        <div className="card-back" onClick={handleClick}></div>
       </div>
     </div>
   );
