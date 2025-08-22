a71d56c17fa182eb6a050e134df4f1cae2e3e6d3
Camila Arenas <mcarenasdh@gmail.com>
2025-08-22 16:26:09 +0200

    Feature(Navbar) Hamburger when responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,37 @@
-import './Navbar.css'
+import "./Navbar.css";
 import { Link } from "react-router-dom";
-function Navbar (){
-    return(
-        <>
-       <nav>
-        <Link className="navbar-button" to="/">Home</Link> 
-        <Link className="navbar-button" to="/CardsPage">Conoce la Baraja</Link>
-        <Link className="navbar-button" to="/reading">Conoce tu Suerte</Link>
-        </nav> 
-        </>
-    )
+import { useState } from "react";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen((v) => !v);
+  const close = () => setIsOpen(false);
+
+  return (
+    <nav>
+      <button
+        className={`menu-toggle ${isOpen ? "is-open" : ""}`}
+        aria-label={isOpen ? "Cerrar menú" : "Abrir menú"}
+        aria-expanded={isOpen}
+        onClick={toggle}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <div className={`nav-links ${isOpen ? "open" : ""}`}>
+        <Link className="navbar-button" to="/" onClick={close}>
+          Home
+        </Link>
+        <Link className="navbar-button" to="/CardsPage" onClick={close}>
+          Conoce la Baraja
+        </Link>
+        <Link className="navbar-button" to="/reading" onClick={close}>
+          Conoce tu Suerte
+        </Link>
+      </div>
+    </nav>
+  );
 }
-export default Navbar
+export default Navbar;
